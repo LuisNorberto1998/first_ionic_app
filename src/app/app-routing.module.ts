ef6207f7552e8aa60398794e71e0f1b2eb5d4b59
Norberto P. Rodriguez <luis.paloma@ids.com.mx>
2024-01-12 15:24:16 -0600

    8.- Creación de componentes: Main y Auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
+  },
 ];
 
 @NgModule({
